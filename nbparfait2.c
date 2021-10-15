844587de8643b4cc7b06af457a13401797752240
Unten95 <91313727+Unten95@users.noreply.github.com>
2021-10-15 11:07:01 +0200

    Add files via upload

---
+++ nbparfait2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main (int argc, char *argv[]){
+
+
+int somme ;
+somme =0;
+int i;
+  atoi(argv[1]);
+  
+  for(i = 1; i < argv[1]; ++i){
+    if(atoi(argv[1])%i == 0){
+      somme = somme + i;
+    }
+  }
+  if(somme == atoi(argv[1])){
+    printf(" Nombre parfait");
+  }  
+  else{
+  return 0;
+}
